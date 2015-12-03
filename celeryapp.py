623339b0427d0dd2663e8cb7a817c1771c689dc0
yuz989 <yuz989@gmail.com>
2015-12-03 15:12:54 +0800

    m

--- celeryapp.py
+++ celeryapp.py
@@ -3,10 +3,12 @@ from celery import Celery
 
 class Celery_Config:
 
-    BROKER_URL = 'redis://%s:6379/0' % os.environ['REDIS_HOST']
+    BROKER_URL            = 'redis://%s:6379/0' % os.environ['REDIS_HOST']
+    CELERY_RESULT_BACKEND = 'redis://%s:6379/0' % os.environ['REDIS_HOST']
 
 
-app = Celery('tasks', broker=Celery_Config.BROKER_URL)
+app = Celery('tasks')
+app.config_from_object('Celery_Config')
 
 @app.task(name='task_queue.celeryapp.test')
 def test():
