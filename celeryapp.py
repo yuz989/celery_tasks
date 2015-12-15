d8f932269745236a2e3b9c98cd7f036bf60bcb40
yuz989 <yuz989@gmail.com>
2015-12-16 00:32:17 +0800

    sqlalchemy integration

--- celeryapp.py
+++ celeryapp.py
@@ -10,7 +10,6 @@ app = Celery('tasks')
 app.config_from_object(Celery_Config)
 
 
-
 ## IMPORTANT: move to somewhere else
 
 import smtplib
@@ -54,8 +53,52 @@ def send_email(receiver=None, title='', template_file=None, **kwargs):
     _smtp_sendMail(receiver, title, context)
 
 
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Text, create_engine
+from sqlalchemy.types import Unicode
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+
+Base = declarative_base()
+
+class Trec(Base):
+
+    __tablename__ = 'trec'
+
+    id = Column(Integer, primary_key=True)
+    tcode = Column(String(64)) # class id
+    start_time = Column(DateTime)
+    end_time = Column(DateTime)
+    lib_book_id = Column(Integer, ForeignKey('library_book.id'))
+    owner_id = Column(Integer, ForeignKey('user.id'))
+    memo = Column(Text)
+    num_tusers = Column(Integer)
+    auth_key = Column(String(32))
+
+    lib_book = relationship('LibraryBook')
+    owner = relationship('User')
+
+class TUser(Base):
+
+    __tablename__ = 'tuser'
+
+    id = Column(Integer, primary_key=True)
+    join_dtime = Column(DateTime)
+    user_nickname = Column(Unicode(64))
+    identity = Column(String(32))
+    trec_id  = Column(Integer, ForeignKey('trec.id'))
+    auth_key = Column(String(32))
+
+class DatabaseConfig:
+    SQLALCHEMY_DATABASE_URI = os.environ['SQLALCHEMY_DATABASE_URI']
+
+engine = create_engine(DatabaseConfig.SQLALCHEMY_DATABASE_URI)
+Base.metadata.bind = engine
+DBSession = sessionmaker(bind=engine)
+session = DBSession()
+
 @app.task(name='task_queue.scheduler.test')
 def test():
-    print 'test'
-
-
+    table = Trec.__table__
+    session.execute(table.update().
+                       where(table.c.id==96).values(memo='test'))
+    print 'complete'
